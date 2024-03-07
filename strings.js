31dd3e5aa49c86d5e6ad756308e2180721121926
Susanmwende <mwendezmasaku@gmail.com>
2024-03-07 11:55:25 +0300

    javascript

---
+++ strings.js
@@ -0,0 +1,19 @@
+// Question 1
+const str = "extravaganza"
+console.log (str.substring(8))
+
+// Question 2
+const food = `The quick fox jumped over the lazy dog`
+let d = "eat"
+const e = `The ${d} quick fox jumped over the lazy dog`
+
+console.log(d)
+
+
+// Question 3
+const story = "The quick brown fox jumps over the lazy dog"
+let count =(story.match(/the/g)) || [].length;
+console.log(story.match(/the/g)) || [].length;
+console.log(story.match(/brown/g))|| [] .length
+
+// question4
